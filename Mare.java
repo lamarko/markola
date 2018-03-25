eb92a6238bd1dfe36a63fda6d2a0e929fa324e4b
lamarko <34462424+lamarko@users.noreply.github.com>
2018-03-25 18:27:58 +0200

    Create Mare.java

---
+++ Mare.java
@@ -0,0 +1,5 @@
+public class Mare{
+        public static void main(String[] args){
+                System.out.println("Pozdrav MARE :) ");
+                                        }
+                }
